9e223efae8b1575b2d1403eefcd12404db02ac83
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2022-05-21 19:53:10 +0900

    [Silver III] Title: 대칭 차집합, Time: 232 ms, Memory: 3568 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,52 +1,29 @@
 #include<iostream>
-#include<unordered_map>
+#include<algorithm>
+#include<vector>
 
 using namespace std;
 
-unordered_map<int, bool> mySub(unordered_map<int, bool> A, unordered_map<int, bool> B)  // A - B
-{
-	unordered_map<int, bool> AsubB;
-	for (auto elem : A)
-	{
-		if (B[elem.first] == false) AsubB[elem.first] = true;;
-	}
-	return AsubB;
-}
-
-unordered_map<int, bool> myAdd(unordered_map<int, bool> A, unordered_map<int, bool> B)
-{
-	unordered_map<int, bool> AaddB;
-	for (auto elem : A)
-		AaddB[elem.first] = elem.second;
-	for (auto elem : B)
-		AaddB[elem.first] = elem.second;
-
-	return AaddB;
-}
+vector<int> A;
 
 int main()
 {
-	int N, M, num;
-	cin >> N >> M;
-
-	unordered_map<int, bool> A;
-	unordered_map<int, bool> B;
+	int N, M, temp, same = 0;
+	cin >> N >>  M;
 
 	for (int i = 0; i < N; i++)
 	{
-		cin >> num;
-		A[num] = true;
+		cin >> temp;
+		A.push_back(temp);
 	}
+	sort(A.begin(), A.end());
+
 	for (int i = 0; i < M; i++)
 	{
-		cin >> num;
-		B[num] = true;
+		cin >> temp;
+		int index = lower_bound(A.begin(), A.end(), temp) - A.begin();
+
+		if (index != A.size() && A[index] == temp) same++;
 	}
-
-	unordered_map<int, bool> AsubB = mySub(A, B);
-	unordered_map<int, bool> BsubA = mySub(B, A);
-
-	unordered_map<int, bool> ans = myAdd(AsubB, BsubA);
-
-	cout << ans.size();
+	cout << N - same + M - same;
 }
