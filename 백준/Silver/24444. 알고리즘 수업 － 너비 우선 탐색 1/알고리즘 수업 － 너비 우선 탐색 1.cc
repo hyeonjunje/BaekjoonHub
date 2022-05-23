49cd8cd8e1aec98e1f5b2e09c1aaf63ed420b3ea
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2022-05-24 03:35:14 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 88 ms, Memory: 9648 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+bool visited[100001]{};
+int visitOrder[100001]{};
+vector<int> graph[100001]{};
+int order = 1;
+void bfs(int start)
+{
+	queue<int> q;
+	q.push(start);
+	visited[start] = true;
+	visitOrder[start] = order++;
+	while (!q.empty())
+	{
+		int cur = q.front();
+		q.pop();
+
+		for (int node : graph[cur])
+		{
+			if (visited[node] == false)
+			{
+				visited[node] = true;
+				visitOrder[node] = order++;
+				q.push(node);
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N, M, R, u, v;
+	cin >> N >> M >> R;
+
+	for (int i = 0; i < M; i++)
+	{
+		cin >> u >> v;
+
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		sort(graph[i].begin(), graph[i].end());
+	}
+
+	bfs(R);
+
+	for (int i = 1; i <= N; i++)
+	{
+		cout << visitOrder[i] << '\n';
+	}
+}
