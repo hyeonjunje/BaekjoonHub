e524389af1fec407e24d6b36ddbfffb51b14ee31
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2022-05-24 01:34:55 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 104 ms, Memory: 11024 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+bool visited[100001]{};
+int visitOrder[100001]{};
+vector<int> graph[100001]{};
+
+int order = 1;
+
+void dfs(int start)
+{
+	visited[start] = true;
+	visitOrder[start] = order++;
+	for (int node : graph[start])
+	{
+		if (visited[node] == false)
+		{
+			dfs(node);
+		}
+	}
+	
+}
+
+bool com(int a, int b)
+{
+	return a < b ? false : true;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N, M, R, u, v;
+	cin >> N >> M >> R;
+
+	for (int i = 0; i < M; i++)
+	{
+		cin >> u >> v;
+
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+	for (int i = 1; i <= N; i++)
+		sort(graph[i].begin(), graph[i].end(), com);
+
+	dfs(R);
+
+	for (int i = 1; i <= N; i++)
+		cout << visitOrder[i] << '\n';
+}
