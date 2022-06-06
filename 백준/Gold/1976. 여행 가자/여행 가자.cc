df17fc209336cd7fa6f6f8fae94eeb03200d635d
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2022-06-06 14:31:16 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int root[201]{};
+int Rank[201]{};
+
+int Find(int x)
+{
+	if (root[x] == x) return x;
+
+	return root[x] = Find(root[x]);
+}
+
+void Union(int a, int b)
+{
+	a = Find(a), b = Find(b);
+
+	if (Rank[a] < Rank[b]) root[a] = b;
+	else root[b] = a;
+
+	if (Rank[a] == Rank[b]) Rank[a]++;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N, M, city;
+	cin >> N >> M;
+
+	for (int i = 0; i <= N; i++)
+	{
+		root[i] = i;
+		Rank[i] = 1;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> city;
+			if (city == 1) Union(i + 1, j + 1);
+		}
+	}
+
+	vector<int> v;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> city;
+		v.push_back(city);
+	}
+
+	if (v.size() <= 1) { cout << "YES"; return 0; }
+
+	for (int i = 1; i < v.size(); i++)
+	{
+		if (root[v[0]] != root[v[i]]) { cout << "NO"; return 0; }
+	}
+	cout << "YES";
+}
