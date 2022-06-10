c42f6a4c625fda29ca620688fe7f6e361865bfd6
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2022-06-10 11:43:45 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string.h>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int T, N, M, a, b;
+	cin >> T;
+	while (T--)
+	{
+		cin >> N >> M;
+		for (int i = 0; i < M; i++)
+		{
+			cin >> a >> b;
+		}
+		cout << N - 1 << '\n';
+	}
+}
