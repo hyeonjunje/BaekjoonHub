682f13f6b68219dc84232e5c18779e6c5a547753
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2022-06-17 08:58:04 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 1; i < N; i++)
+	{
+		for (int h = 0; h < i - 1; h++) cout << ' ';
+		for (int h = 0; h < N - i + N - i + 1; h++) cout << '*';
+		cout << '\n';
+	}
+
+	for (int i = N; i >= 1; i--)
+	{
+		for (int h = 0; h < i - 1; h++) cout << ' ';
+		for (int h = 0; h < N - i + N - i + 1; h++) cout << '*';
+
+		cout << '\n';
+	}
+}
