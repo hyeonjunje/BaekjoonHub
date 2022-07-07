99250164792e2bbf3d8f988b1cebfdda4cfb2b91
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2022-07-08 07:30:27 +0900

    [Gold I] Title: 합성함수와 쿼리, Time: 244 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.cc
@@ -0,0 +1,57 @@
+//효율적인 LCA 구현을 위해 필요한 sparse table 자료구조를 배워 봅시다.
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+#define MAXLOG 19
+#define MAX 200001
+
+int y[MAX][MAXLOG];  // x를 2^(i)번 f한 결과
+int f[MAX];
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int m, Q, n, x;
+	cin >> m;
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> f[i];
+		y[i][0] = f[i];
+	}
+
+	for (int j = 1; j < MAXLOG; j++)
+	{
+		for (int i = 1; i <= m; i++)
+		{
+			y[i][j] = y[y[i][j - 1]][j - 1];
+		}
+	}
+
+	cin >> Q;
+	while (Q--)
+	{
+		cin >> n >> x;
+
+		int tmp = 0;
+		for (int i = MAXLOG - 1; i >= 0; i--)
+		{
+			if (n >= pow(2, i))
+			{
+				tmp = i;
+				break;
+			}
+		}
+
+		for (int i = 0; i <= tmp; i++)
+		{
+			if (n % 2) x = y[x][i];
+			n >>= 1;
+		}
+
+		cout << x << '\n';
+	}
+}
