89c013d836e38fb7a56fcc4d2c9231f802c097ae
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-02-14 00:38:51 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -1,80 +1,56 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <queue>
+#include<string>
+#include<vector>
 #include<algorithm>
+#include<queue>
+#include<iostream>
 
 using namespace std;
 
-bool comp(vector<int> a, vector<int> b)
+struct comp
 {
-    if (a[1] < b[1])
-        return true;
-    else if (a[1] == b[1])
-        return a[0] < b[0];
-    else
-        return false;
-}
+	bool operator()(vector<int> a, vector<int> b)
+	{
+		// 소요시간 기준 오름차순 (특이한 건 b가 기준임)
+        return a[1] > b[1];
+		if (a[1] > b[1])
+			return true;
+		else if (a[1] == b[1])
+			return a[0] > b[0];
+		else
+			return false;
+	}
+};
 
-int solution(vector<vector<int>> jobs) {
-    int answer = 0;
-    int currentTime = 0;
+int solution(vector<vector<int>> jobs)
+{
+	int totalTime = 0;
+	int currentTime = 0;
+	int taskIndex = 0;
+	sort(jobs.begin(), jobs.end());
 
-    //priority_queue<vector<int>, vector<vector<int>, compQueue> pq;
-    vector<vector<int>> v;
-    sort(jobs.begin(), jobs.end(), comp);
+	priority_queue<vector<int>, vector<vector<int>>, comp> pq;
 
-    int index = 0;
-    while (jobs.size() != 0)
-    {
-        int minJobs = 1001;
-        int minIndex = -1;
+	while (taskIndex < jobs.size() || !pq.empty())
+	{
+		if (taskIndex < jobs.size() && currentTime >= jobs[taskIndex][0])
+		{
+			pq.push(jobs[taskIndex]);
+			taskIndex++;
+			continue;
+		}
 
-        bool check = false;
-        for (int i = 0; i < jobs.size(); i++)
-        {
-            if (currentTime < jobs[i][0])
-            {
-                if (minJobs > jobs[i][0])
-                {
-                    minJobs = jobs[i][0];
-                    minIndex = i;
-                }
-                continue;
-            }
-            else
-            {
-                check = true;
-                currentTime += jobs[i][1];
-                v.push_back(jobs[i]);
-                jobs.erase(jobs.begin() + i);
-                break;
-            }
-        }
+		if (!pq.empty())
+		{
+			currentTime += pq.top()[1];
+			totalTime += currentTime - pq.top()[0];
+			pq.pop();
+		}
+		else
+		{
+			currentTime = jobs[taskIndex][0];
+		}
+	}
 
-        if (!check)
-        {
-            currentTime = jobs[minIndex][0] + jobs[minIndex][1];
-            v.push_back(jobs[minIndex]);
-            jobs.erase(jobs.begin() + minIndex);
-        }
-    }
-
-    currentTime = 0;
-
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (v[i][0] < currentTime)
-        {
-            answer += v[i][1] + currentTime - v[i][0];
-            currentTime += v[i][1];
-        }
-        else
-        {
-            answer += v[i][1];
-            currentTime = v[i][1] + v[i][0];
-        }
-    }
-    answer /= v.size();
-    return answer;
+	totalTime /= jobs.size();
+	return totalTime;
 }
