002e84cfadea0b83e797e126203e7bfe7a480823
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-02-14 01:09:13 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -54,3 +54,84 @@ int solution(vector<vector<int>> jobs)
 	totalTime /= jobs.size();
 	return totalTime;
 }
+
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//#include <queue>
+//#include<algorithm>
+//
+//using namespace std;
+//
+//bool comp(vector<int> a, vector<int> b)
+//{
+//    if (a[1] < b[1])
+//        return true;
+//    else if (a[1] == b[1])
+//        return a[0] < b[0];
+//    else
+//        return false;
+//}
+//
+//int solution(vector<vector<int>> jobs) {
+//    int answer = 0;
+//    int currentTime = 0;
+//
+//    //priority_queue<vector<int>, vector<vector<int>, compQueue> pq;
+//    vector<vector<int>> v;
+//    sort(jobs.begin(), jobs.end(), comp);
+//
+//    int index = 0;
+//    while (jobs.size() != 0)
+//    {
+//        int minJobs = 1001;
+//        int minIndex = -1;
+//
+//        bool check = false;
+//        for (int i = 0; i < jobs.size(); i++)
+//        {
+//            if (currentTime < jobs[i][0])
+//            {
+//                if (minJobs > jobs[i][0])
+//                {
+//                    minJobs = jobs[i][0];
+//                    minIndex = i;
+//                }
+//                continue;
+//            }
+//            else
+//            {
+//                check = true;
+//                currentTime += jobs[i][1];
+//                v.push_back(jobs[i]);
+//                jobs.erase(jobs.begin() + i);
+//                break;
+//            }
+//        }
+//
+//        if (!check)
+//        {
+//            currentTime = jobs[minIndex][0] + jobs[minIndex][1];
+//            v.push_back(jobs[minIndex]);
+//            jobs.erase(jobs.begin() + minIndex);
+//        }
+//    }
+//
+//    currentTime = 0;
+//
+//    for (int i = 0; i < v.size(); i++)
+//    {
+//        if (v[i][0] < currentTime)
+//        {
+//            answer += v[i][1] + currentTime - v[i][0];
+//            currentTime += v[i][1];
+//        }
+//        else
+//        {
+//            answer += v[i][1];
+//            currentTime = v[i][1] + v[i][0];
+//        }
+//    }
+//    answer /= v.size();
+//    return answer;
+//}
