afe18fa3e2d7ddaf2ef1dd97ffb85fa0016f102b
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-03-05 18:22:29 +0900

    [level 3] Title: N으로 표현, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
@@ -1,44 +1,40 @@
-#include<iostream>
+// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
+// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <math.h>
-#include<set>
+#include <unordered_set>
 
 using namespace std;
 
-vector<int> dp[9];
-
-int solution(int N, int number) {
-    int answer = 0;
-
-    dp[1].push_back(N);
-
-    for (int i = 2; i <= 8; i++)
-    {
-        dp[i].push_back(dp[i - 1][0] * 10 + N);
-        for (int j = 1; j <= i - 1; j++)
-        {
-            for (int first = 0; first < dp[j].size(); first++)
-            {
-                for (int second = 0; second < dp[i - j].size(); second++)
-                {
-
-                    dp[i].push_back(dp[j][first] + dp[i - j][second]);
-                    dp[i].push_back(dp[j][first] - dp[i - j][second]);
-                    dp[i].push_back(dp[j][first] * dp[i - j][second]);
-                    if (dp[i - j][second] != 0)
-                        dp[i].push_back(dp[j][first] / dp[i - j][second]);
-                }
+int N;
+unordered_set<int> cache[10];
+unordered_set<int> solve(int n) {
+    if (!cache[n].empty()) return cache[n];
+    int num = 0;
+    for (int i = 0; i < n; i++) num = 10 * num + N;
+    unordered_set<int> res;
+    res.insert(num);
+    for (int i = 1; i < n; i++) {
+        int j = n - i;
+        auto s1 = solve(i);
+        auto s2 = solve(j);
+        for (int n1 : s1) {
+            for (int n2 : s2) {
+                res.insert(n1 + n2);
+                res.insert(n1 - n2);
+                res.insert(n1 * n2);
+                if (n2 != 0) res.insert(n1 / n2);
             }
         }
     }
+    return cache[n] = res;
+}
 
-    for (int i = 1; i <= 8; i++)
-    {
-        for (int num : dp[i])
-            if (num == number)
-                return i;
+int solution(int _N, int number) {
+    N = _N;
+    for (int i = 1; i <= 8; i++) {
+        solve(i);
+        if (cache[i].find(number) != cache[i].end()) return i;
     }
     return -1;
 }
