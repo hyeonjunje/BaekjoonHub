8efdc2b81ede0414e75800daaffaf4e1a6ad909a
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-03-05 18:25:05 +0900

    [level 3] Title: N으로 표현, Time: 14.89 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
@@ -1,40 +1,39 @@
-// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
-// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
 #include <string>
 #include <vector>
-#include <unordered_set>
 
 using namespace std;
 
-int N;
-unordered_set<int> cache[10];
-unordered_set<int> solve(int n) {
-    if (!cache[n].empty()) return cache[n];
-    int num = 0;
-    for (int i = 0; i < n; i++) num = 10 * num + N;
-    unordered_set<int> res;
-    res.insert(num);
-    for (int i = 1; i < n; i++) {
-        int j = n - i;
-        auto s1 = solve(i);
-        auto s2 = solve(j);
-        for (int n1 : s1) {
-            for (int n2 : s2) {
-                res.insert(n1 + n2);
-                res.insert(n1 - n2);
-                res.insert(n1 * n2);
-                if (n2 != 0) res.insert(n1 / n2);
-            }
+int minCnt = 9;
+
+void dp(int N, int number, int cnt, int now){
+    if(cnt > 8){
+        return;
+    }
+
+    if(now == number){
+        minCnt = min(minCnt, cnt);
+    }
+
+    int operand = 0;
+
+    for(int i=1; i<=9; i++){
+        //5, 55, 555 만들기
+        operand = operand*10 + N; 
+
+        dp(N, number, cnt + i, now + operand);
+        dp(N, number, cnt + i, now - operand);
+
+        if(now != 0){
+            dp(N, number, cnt + i, now * operand);
+            dp(N, number, cnt + i, now / operand);
         }
     }
-    return cache[n] = res;
-}
 
-int solution(int _N, int number) {
-    N = _N;
-    for (int i = 1; i <= 8; i++) {
-        solve(i);
-        if (cache[i].find(number) != cache[i].end()) return i;
-    }
-    return -1;
+
+}
+int solution(int N, int number) {
+    int answer = 0;
+    dp(N, number, 0, 0);
+    answer = ((minCnt > 8) ? -1 : minCnt);
+    return answer;
 }
