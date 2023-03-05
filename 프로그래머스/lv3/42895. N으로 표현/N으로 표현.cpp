93fec9ebad6f6598384003b3513f83b146262f4d
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2023-03-05 18:20:41 +0900

    [level 3] Title: N으로 표현, Time: 5.91 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
@@ -1,38 +1,45 @@
+#include<iostream>
 #include <string>
 #include <vector>
-#include <unordered_set>
+#include <algorithm>
+#include <math.h>
+#include<set>
 
 using namespace std;
 
-int N;
-unordered_set<int> cache[10];
-unordered_set<int> solve(int n) {
-    if (!cache[n].empty()) return cache[n];
-    int num = 0;
-    for (int i = 0; i < n; i++) num = 10 * num + N;
-    unordered_set<int> res;
-    res.insert(num);
-    for (int i = 1; i < n; i++) {
-        int j = n - i;
-        auto s1 = solve(i);
-        auto s2 = solve(j);
-        for (int n1 : s1) {
-            for (int n2 : s2) {
-                res.insert(n1 + n2);
-                res.insert(n1 - n2);
-                res.insert(n1 * n2);
-                if (n2 != 0) res.insert(n1 / n2);
+set<int> dp[9];
+
+int solution(int N, int number) {
+    int answer = 0;
+
+    dp[1].insert(N);
+
+    for (int i = 2; i <= 8; i++)
+    {
+        int n = 0;
+        for (int temp = 0; temp < i; temp++) n = 10 * n + N;
+        dp[i].insert(n);
+        for (int j = 1; j <= i - 1; j++)
+        {
+            for (int first : dp[j])
+            {
+                for (int second : dp[i - j])
+                {
+                    dp[i].insert(first + second);
+                    dp[i].insert(first - second);
+                    dp[i].insert(first * second);
+                    if(second != 0)
+                        dp[i].insert(first / second);
+                }
             }
         }
     }
-    return cache[n] = res;
-}
 
-int solution(int _N, int number) {
-    N = _N;
-    for (int i = 1; i <= 8; i++) {
-        solve(i);
-        if (cache[i].find(number) != cache[i].end()) return i;
+    for (int i = 1; i <= 8; i++)
+    {
+        for (int num : dp[i])
+            if (num == number)
+                return i;
     }
     return -1;
 }
