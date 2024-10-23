57e10364df6a3cec3dad36fc9ffca125c34f176e
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2024-10-24 02:07:47 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241024/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int maze[100][100];
+bool visited[100][100];
+
+int result = 0;
+
+int dx[2] = {0, 1};
+int dy[2] = {1, 0}; 
+
+void dfs(int x, int y)
+{
+    if(x == m && y == n)
+    {
+        result = 1;
+    }
+
+    for(int i = 0; i < 2; ++i)
+    {
+        int curX = x + dx[i];
+        int curY = y + dy[i];
+
+        if(curY >= n || curX >= m)
+        {
+            continue;
+        }
+
+        if(maze[curY][curX] == 0)
+        {
+            continue;
+        }
+
+        if(!visited[y][x])
+        {
+            visited[y][x] = true;
+            dfs(curX, curY);
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < m; ++j)
+        {
+            cin >> maze[i][j];
+        }
+    }
+
+    visited[0][0] = true;
+    dfs(0, 0);
+    cout << result;
+    return 0;
+}
