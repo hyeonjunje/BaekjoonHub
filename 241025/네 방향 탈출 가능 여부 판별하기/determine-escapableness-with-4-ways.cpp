34fbaf631fdb9ab3b8698bb02e37b21a29cabe43
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2024-10-25 05:37:26 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 378ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241025/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <utility>
+#include <queue>
+
+using namespace std;
+#define MAX 100
+
+int n, m;
+int arr[MAX][MAX];
+bool visited[MAX][MAX];
+
+bool CanGo(int x, int y)
+{
+    return x >= 0 && x < m && y >= 0 && y < n;
+}
+
+int bfs(pair<int, int> p)
+{
+    int dx[4] = {0, 0, -1, 1};
+    int dy[4] = {1, -1, 0, 0};
+    queue<pair<int, int>> q;
+    q.push(p);
+
+    while(!q.empty())
+    {
+        pair<int, int> current = q.front();
+        q.pop();
+        visited[current.second][current.first] = true;
+
+        if(current.first == m - 1 && current.second == n - 1)
+        {
+            return 1;
+        }
+
+        for(int i = 0; i < 4; ++i)
+        {
+            int nextX = current.first + dx[i];
+            int nextY = current.second + dy[i];
+
+            if(!CanGo(nextX, nextY))
+            {
+                continue;
+            }
+
+            if(visited[nextY][nextX])
+            {
+                continue;
+            }
+
+            if(arr[nextY][nextX] == 0)
+            {
+                continue;
+            }
+
+            q.push(make_pair(nextX, nextY));
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin >> n >> m;
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j <n; ++j)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << bfs({0, 0});
+    return 0;
+}
