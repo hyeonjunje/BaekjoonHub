0f345312386b7464c515703123a5c55b4b74faeb
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2024-10-26 01:55:14 +0900

    Title: 최대 감소 부분 수열, Time: 1043ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241026/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MAX 1000
+
+bool BinarySearch(const vector<int>& v, int target)
+{
+    int right = v.size() - 1;
+    int left = 0;
+    while(left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if(v[mid] > target)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid;
+        }
+    }
+
+    return right;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    cin >> n;
+    int arr[MAX] = {0};
+    for(int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+    
+    vector<int> v;
+    v.push_back(arr[0]);
+
+    for(int i = 0; i < n; ++i)
+    {
+        if(v[v.size() - 1] > arr[i])
+        {
+            v.push_back(arr[i]);
+        }
+        else
+        {
+            int index = BinarySearch(v, arr[i]);
+            v[index] = arr[i];
+        }
+    }
+
+    cout << v.size();
+    return 0;
+}
