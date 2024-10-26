cb5bd59a82c639f6bd206289f25d0f5805a1101e
hyeonjunje <88482159+hyeonjunje@users.noreply.github.com>
2024-10-26 16:18:11 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241026/연속 부분 합의 최댓값 구하기/max-of-partial-sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 100000
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+    int arr[MAX]{0};
+    int dp[MAX]{0};
+    cin >> n;
+    for(int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+    dp[0] = arr[0];
+    for(int i = 1; i < n; ++i)
+    {
+        dp[i] = max(dp[i - 1] + arr[i], arr[i]);
+    }
+
+    int result = 0;
+    for(int vv : dp)
+    {
+        result = max(result, vv);
+    }
+    cout << result;
+
+    return 0;
+}
